1d8bf9a96f11791474d700de13e890fd8210bf6e
obafisayo <sekaniisjoy@gmail.com>
2024-07-24 01:56:23 +0100

    eslint init

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-    return new Promise(function (resolve, reject) {
-        resolve("API response");
-    });
+  return new Promise((resolve, reject) => {
+    resolve('done');
+  });
 }
