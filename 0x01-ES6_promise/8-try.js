8824dcc54f6ee2aef249753f3f5a186b0c3ccad5
obafisayo <sekaniisjoy@gmail.com>
2024-07-25 03:05:55 +0100

    add 9 and 8

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,13 +1,9 @@
 export default function divideFunction(numerator, denominator) {
   let division;
-  try {
-    if (denominator === 0) {
-      throw Error('cannot divide by 0');
-    } else {
-      division = numerator / denominator;
-    }
-  } catch (error) {
-    console.log(error);
-  }
+	if (denominator === 0) {
+		throw Error('cannot divide by 0');
+	} else {
+		division = numerator / denominator;
+	}
   return division;
 }
