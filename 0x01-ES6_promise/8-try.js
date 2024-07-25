2508672f88906b212fbdebd6217bc80e4ef74b42
obafisayo <sekaniisjoy@gmail.com>
2024-07-25 03:09:39 +0100

    8 eslint corrected

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,9 +1,9 @@
 export default function divideFunction(numerator, denominator) {
   let division;
-	if (denominator === 0) {
-		throw Error('cannot divide by 0');
-	} else {
-		division = numerator / denominator;
-	}
+  if (denominator === 0) {
+    throw Error('cannot divide by 0');
+  } else {
+    division = numerator / denominator;
+  }
   return division;
 }
